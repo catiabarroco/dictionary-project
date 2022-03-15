e8fc871bc736493a0774df0a174b232c22cda7da
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-15 12:48:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} alt="logo shecodes" className="App-logo img-fluid" />
+      
+      </header>
+    </div>
+  );
+}
+
