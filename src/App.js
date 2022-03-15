548b05352a8eeee1fc81d853b5f4add88f153023
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-15 13:35:20 +0000

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-      <img src={logo} alt="logo shecodes" className="App-logo img-fluid" />
-      
+        <img src={logo} alt="logo shecodes" className="App-logo img-fluid" />
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer"><small>Coded by CatisCoding</small></footer>
+      </div>
     </div>
   );
 }
-
