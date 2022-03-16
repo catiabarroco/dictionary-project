7cdfbabb289321ea8c639157f9da2934afa27a0a
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-16 13:47:15 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,40 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <img src={logo} alt="logo shecodes" className="App-logo img-fluid" />
-      </header>
-      <main>
-        <Dictionary/>
-      </main>
-      <footer className="App-footer"><small>Coded by CatisCoding</small></footer>
+        <header className="App-header">
+          <img src={logo} alt="logo shecodes" className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/catiabarroco"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Cátia Barroco (Catis Coding)
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/catiabarroco/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://keen-jennings-86afd8.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
