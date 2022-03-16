103ea584581c31e403bb8a728463e2079dd39536
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-16 13:42:03 +0000

    added css style

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,18 @@
-//import React from "react";
+import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h3 className="mt-4 border">{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
+            <p className="definitionNumber mt-4">{index + 1} Definition</p>
             {definition.definition}
             <br />
-            <small>{definition.example}</small>
+            <small className="examles mt-2">{definition.example}</small>
           </div>
         );
       })}
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import "./Results.css"
 
 export default function Results(props) {
   if (props.results === null) {
@@ -7,7 +8,7 @@ export default function Results(props) {
   } else {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <h2 className="shadow border">{props.results.word}</h2>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
